d2b43cc506af9f0887cd064ff84abf4572ceb4d6
mohitbarne <72339227+mohitbarne@users.noreply.github.com>
2020-10-27 17:27:24 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,29 @@
+class Dustbin {
+  constructor(x, y, width, height) {
+    var options = {
+        isStatic:true,
+        'restitution':0.8,
+        'friction':2.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+   
+    translate(pos.x, pos.y)
+    rotate (angle)
+    strokeWeight(1)
+    stroke("white")
+    rectMode(CENTER);
+    fill("red");
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
